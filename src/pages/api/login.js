f6687a264e97ebd4b7a71e3f3d62990f22bfce74
faixanmalik <faixan.malik554@gmail.com>
2023-10-22 19:56:00 +0500

    remove security in login api

--- src/pages/api/login.js
+++ src/pages/api/login.js
@@ -12,10 +12,7 @@ const handler = async (req,res)=>{
         let user = await User.findOne({"email": req.body.email})
         if (user){
             // Decryptpassword
-            var bytes  = CryptoJS.AES.decrypt(user.password, process.env.CRYPTOJS_SECRET);
-            var encryptedPassword = bytes.toString(CryptoJS.enc.Utf8);
-
-            if (req.body.email === user.email && req.body.password === encryptedPassword){
+            if (req.body.email === user.email && user.password === req.body.password){
                 let token = jwt.sign({ email:user.email, name:user.name}, process.env.JWT_SECRET);
                 res.status(200).json({ success: true, message: "Succesfully Log In!", token, email:user.email, businessName: user.businessName, department: 'Admin' })
             }
@@ -24,6 +21,22 @@ const handler = async (req,res)=>{
             }
         }
     }
+    // if (req.method == 'POST'){
+    //     let user = await User.findOne({"email": req.body.email})
+    //     if (user){
+    //         // Decryptpassword
+    //         var bytes  = CryptoJS.AES.decrypt(user.password, process.env.CRYPTOJS_SECRET);
+    //         var encryptedPassword = bytes.toString(CryptoJS.enc.Utf8);
+
+    //         if (req.body.email === user.email && req.body.password === encryptedPassword){
+    //             let token = jwt.sign({ email:user.email, name:user.name}, process.env.JWT_SECRET);
+    //             res.status(200).json({ success: true, message: "Succesfully Log In!", token, email:user.email, businessName: user.businessName, department: 'Admin' })
+    //         }
+    //         else{
+    //             res.status(400).json({ success: false, message: "Invalid Credentials!" })
+    //         }
+    //     }
+    // }
     else{
         res.status(400).json({ success: false, message: "Internal Server Error!" }) 
     }
