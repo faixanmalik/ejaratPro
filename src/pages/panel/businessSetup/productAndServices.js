149e435d49302494f0b9ed3daf7fb5a1b9539d36
faixanmalik <faixan.malik554@gmail.com>
2023-11-21 22:30:01 +0500

    adding check with product and services

--- src/pages/panel/businessSetup/productAndServices.js
+++ src/pages/panel/businessSetup/productAndServices.js
@@ -103,11 +103,11 @@ const ProductAndServices = ({product, charts, dbTaxRate}) => {
       }
   }
 
-  function handleLinkedCheque(e) {
+  function handleLinkedCheck(e) {
     if (e.target.checked) {
-      setLinkedCheque(true);
+      setLinkedCheck(true);
     } else {
-      setLinkedCheque(false);
+      setLinkedCheck(false);
     }
   }
 
@@ -121,10 +121,10 @@ const ProductAndServices = ({product, charts, dbTaxRate}) => {
   
   const [costPrice, setCostPrice] = useState('')
   const [linkAccount, setLinkAccount] = useState('')
-  const [linkContract, setLinkContract] = useState('')
+  const [linkContract, setLinkContract] = useState(false)
   const [purchaseTaxRate, setPurchaseTaxRate] = useState('')
   const [desc, setdesc] = useState('')
-  const [linkedCheque, setLinkedCheque] = useState('')
+  const [linkedCheck, setLinkedCheck] = useState('')
   
   
   const [salesStatus, setSalesStatus] = useState('')
@@ -487,40 +487,38 @@ const ProductAndServices = ({product, charts, dbTaxRate}) => {
                           </div>
 
 
-                          <div className="col-span-2">
-                            {linkedCheque ? <div>
+                          <div className='col-span-4 flex'>
+
+                            <div className="w-1/4 flex space-x-5 my-auto">
                               <label htmlFor="linkContract" className="block text-sm font-medium text-gray-700">
                                 Link Contract
                               </label>
-                              <select
-                                onChange={handleChange}
-                                value={linkContract}
-                                id="linkContract"
-                                name="linkContract"
-                                className="mt-1 p-2 block w-full rounded-md border border-gray-300 bg-white px-1 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
-                              >
-                                <option>Select account</option>
-                                <option value='unitRent'>Unit Rent</option>
-                                <option value='commission'>Commission</option>
-                                <option value='parkingRent'>Parking Rent</option>
-                                <option value='securityDeposit'>Security Deposit</option>
-                              </select>
-                            </div>: <div className='h-[61px]'>
-
-                              </div>}
+                              <div>
+                                <Switch color="blue" value={linkedCheck} onChange={handleLinkedCheck} />
+                              </div>
+                            </div>
                           </div>
 
-                          <div className="col-span-3 flex space-x-5 my-auto"></div>
-                          
-                          <div className="col-span-1 w-64 flex space-x-5 my-auto">
+                          {linkedCheck ? <div className='col-span-2'>
                             <label htmlFor="linkContract" className="block text-sm font-medium text-gray-700">
                               Link Contract
                             </label>
-                            <div>
-                              <Switch color="blue" value={linkedCheque} onChange={handleLinkedCheque} />
-                            </div>
-                          </div>
-                          
+                            <select
+                              onChange={handleChange}
+                              value={linkContract}
+                              id="linkContract"
+                              name="linkContract"
+                              className="mt-1 p-2 block w-full rounded-md border border-gray-300 bg-white px-1 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                            >
+                              <option>Select account</option>
+                              <option value='unitRent'>Unit Rent</option>
+                              <option value='commission'>Commission</option>
+                              <option value='parkingRent'>Parking Rent</option>
+                              <option value='securityDeposit'>Security Deposit</option>
+                            </select>
+                          </div>: <div className='h-[61px]'> </div>}
+
+
                           <div className="col-span-6">
                             <label htmlFor="desc" className="block text-sm font-medium text-gray-700">
                               Description
@@ -573,12 +571,12 @@ export async function getServerSideProps() {
    
   // Pass data to the page via props
   return {
-     props: { 
-        product: JSON.parse(JSON.stringify(product)),
-        charts: JSON.parse(JSON.stringify(chartsOfAccount)),
-        dbTaxRate: JSON.parse(JSON.stringify(dbTaxRate)),
+    props: { 
+      product: JSON.parse(JSON.stringify(product)),
+      charts: JSON.parse(JSON.stringify(chartsOfAccount)),
+      dbTaxRate: JSON.parse(JSON.stringify(dbTaxRate)),
     } 
-    }
+  }
 }
 
 export default ProductAndServices
