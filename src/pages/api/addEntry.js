7862fa64777d05e99444d42ac4ee1322caa88ffc
faixanmalik <faixan.malik554@gmail.com>
2023-12-11 14:57:17 +0500

    update in addentry api

--- src/pages/api/addEntry.js
+++ src/pages/api/addEntry.js
@@ -423,7 +423,6 @@ export default async function handler(req, res) {
               
             let newEntry = new User( { businessName, email, password, firstName, lastName, } );
             await newEntry.save();
-
             res.status(200).json({ success: true, message: "Entry Added!" }) 
 
           } catch (error) {
